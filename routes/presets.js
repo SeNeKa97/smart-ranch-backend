eeb81abbc8778de7eceeb5577d97872cea278c48
Bogdan Shevchuk <bodjasheva@gmail.com>
2019-06-09 22:39:46 +0300

    Added authorization check logic before mutating presets values

--- routes/presets.js
+++ routes/presets.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 
 const presetController = require('../controllers').preset;
+const authController = require('../controllers').auth;
+
+const ROLE_REQUIRED = 2;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	presetController.getBundle()
@@ -10,12 +13,20 @@ router.get('/', function(req, res, next) {
   		.catch(err => res.status(400).json({error: err}));
 });
 
+
+
 router.post('/update', function(req, res, next) {
 	let bundle = req.body;
+	const token = body.token;
 
-	presetController.updateBundle(bundle)
-  		.then(bundle => { res.json({success: "success"})})
-  		.catch(err => res.status(400).json({error: err}));
+	authController.hasRights(token, ROLE_REQUIRED)
+	.then(result => {
+		if (result === true)
+			presetController.updateBundle(bundle)
+  				.then(bundle => { res.json({success: "success"})})
+  				.catch(err => res.status(400).json({error: err}));
+  	})
+	.catch(err => res.status(400).json({error: err}));
   	//res.json({success: "success"});
 });
 
