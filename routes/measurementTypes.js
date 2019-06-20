468b7815535a761bc852646556e4d2477c883c48
Bogdan Shevchuk <bodjasheva@gmail.com>
2019-06-20 20:39:11 +0300

    Removed redundant comments section

--- routes/measurementTypes.js
+++ routes/measurementTypes.js
@@ -20,41 +20,5 @@ router.get('/:id', function(req, res, next) {
 		.catch(err => res.json({error: err}));
 });
 
-/*
-router.post('/', function(req, res, next) {
-	const body = req.body;
-
-	measurementTypeController.create(body.idType, body.idBoard, body.value)
-		.then(measurementTypes => res.json(measurementTypes))
-		.catch(err => res.json({error: err}));
-});
-
-
-router.post('/update', function(req, res, next) {
-	const body = req.body;
-
-	measurementTypeController.update(body.id, body.name)
-		.then(measurementTypes => res.json(measurementTypes))
-		.catch(err => res.json({error: err}));
-});
-
-router.post('/', function(req, res, next) {
-	const body = req.body;
-
-	measurementTypeController.create(body.name)
-		.then(measurementTypes => res.json(measurementTypes))
-		.catch(err => res.json({error: err}));
-});
-
-
-router.delete('/:id', function(req, res, next) {
-	const id = req.params.id;
-
-	measurementTypeController.delete(id)
-		.then(res.json({success: "measurementType "+id+" deleted"}))
-		.catch(err => res.json({error: err}));
-});
-*/
-
 
 module.exports = router;
